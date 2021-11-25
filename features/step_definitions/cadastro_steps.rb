acc3e5ff10050c7ba667b4cea2af15a3f2a40f93
oebersol <adriano.ebersol@hotmail.com>
2021-11-25 11:06:25 -0300

    Identação dos códigos.

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,52 +1,40 @@
 Dado('que acesso a página de cadastro') do
-    visit "/"
-    click_on "Sign in"
-   
+  visit '/'
+  click_on 'Sign in'
 end
-  
+
 Quando('submeto o meu cadastro com:') do |table|
-    user = table.rows_hash
+  user = table.rows_hash
 
-    find("input[id=email_create]").set user[:email]
+  find('input[id=email_create]').set user[:email]
 
-    click_on "Create an account"
-    find("input[id=id_gender1]").set true
-    find("input[id=customer_firstname]").set user[:nome]
-    find("input[id=customer_lastname]").set user[:ultimo_nome]
-    find("input[id=passwd]").set user[:senha]
-    find('#uniform-days').set value="2"
-    
-    
+  click_on 'Create an account'
+  find('input[id=id_gender1]').set true
+  find('input[id=customer_firstname]').set user[:nome]
+  find('input[id=customer_lastname]').set user[:ultimo_nome]
+  find('input[id=passwd]').set user[:senha]
+  find('#uniform-days').set value = '2'
 
-
-
-    sleep 5
-
+  sleep 5
 end
-  
+
 Então('devo ser redirecionado para a tela inicial') do
-    expect(page). to have_css 'p[class="light-green accent-2"]'
+  expect(page).to have_css 'p[class="light-green accent-2"]'
 end
 
+Quando('submeto o meu cadastro sem o email.') do
+  find('input[id=user_name]').set 'Dollynete'
+  find('input[id=user_lastname]').set 'Orelha'
+  find('input[id=user_address]').set 'Rua Borgussa tentaro'
+  find('input[id=user_university]').set 'EDHA'
+  find('input[id=user_profile]').set 'Agióta'
+  find('input[id=user_gender]').set 'Molier'
+  find('input[id=user_age]').set '122'
 
-Quando('submeto o meu cadastro sem o email.') do 
-    find("input[id=user_name]").set "Dollynete"
-    find("input[id=user_lastname]").set "Orelha"
-    find("input[id=user_address]").set "Rua Borgussa tentaro"
-    find("input[id=user_university]").set "EDHA"
-    find("input[id=user_profile]").set "Agióta"
-    find("input[id=user_gender]").set "Molier"
-    find("input[id=user_age]").set "122"
-
-    click_on "Criar"
-    sleep 5
-
+  click_on 'Criar'
+  sleep 5
 end
-  
+
 Então('devo ver Oops! Informe seu email.') do
-    expect(page). to have_css 'p[class="light-green accent-2"]'
+  expect(page).to have_css 'p[class="light-green accent-2"]'
 end
-
-
-
-
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,25 +1,19 @@
 Dado('que acesso a página de login') do
-    goto_login
+  goto_login
 end
-  
 
 Quando('submeto o meu login com: {string} e {string}') do |email, senha|
-    login_whith(email, senha)
+  login_whith(email, senha)
 end
-  
 
 Então('devo ser redirecionado para a área logada') do
-    expect(page). to have_css "body[id='my-account']"
-
+  expect(page).to have_css "body[id='my-account']"
 end
 
-  
 Então('devo ver a mensagem: {string}') do |expect_message|
-    message = find('.alert-danger')
-    expect(message.text).to have_content expect_message
-
+  message = find('.alert-danger')
+  expect(message.text).to have_content expect_message
 end
-
 
 # usuário cadastrado no site:
 # badroanosobanol@gmail.com senha: 12345
